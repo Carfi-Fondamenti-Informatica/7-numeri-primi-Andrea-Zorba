55488ffb86862ff4cb90ce515af0152ff9e0fa9c
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-11-30 22:01:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,18 @@
 #include "lib.h"
-#include <iostream>
-void numeroprimo(int a, int i, int &conta){
-    int div=0;
-    i++;
-    if(a>1 and a>i){
-        div= a%1;
-        if(div==0){
-            conta++;
-        }else
-        {
-         numeroprimo(a,i, conta);
-        }
+
+int funzione(int a){
+    int  divisori=1, divisori_interi=0;
+
+    while(divisori_interi<=1 && divisori<=a/2) {
+        if(a%divisori==0){
+            divisori_interi++;
+        }divisori++;}
+
+    if (divisori_interi==1)
+    {
+        return 1;
+    } else
+    {
+        return 0;
     }
 }
