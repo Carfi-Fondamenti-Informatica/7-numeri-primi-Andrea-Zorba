f8af0751fc10ed3b1fee33b34724160b21bd5f8a
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-11-30 22:00:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
+using namespace std;
 #include "lib.h"
 
-using namespace std;
 int main() {
-    int numero=0, conta=0;
-    cin>> numero;
-    numeroprimo(numero, 1,  conta);
-    if (conta==0) {
-        cout<< "numero primo"<< endl;
-    }else
-    {
-        cout<< "numero non primo" << endl;
+    int a=0, ris;
+    cin >> a;
+    ris= funzione(a);
+    if (ris==1){
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
     }
     return 0;
 }
