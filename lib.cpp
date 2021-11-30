c95f0c070bc790417b7ffe6bd8ebb8950c1af659
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-11-30 20:53:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
-
+#include "lib.h"
+#include <iostream>
+void numeroprimo(int a, int i, int &conta){
+    int div=0;
+    i++;
+    if(a>1 and a>i){
+        div= a%1;
+        if(div==0){
+            conta++;
+        }else
+        {
+         numeroprimo(a,i, conta);
+        }
+    }
+}
