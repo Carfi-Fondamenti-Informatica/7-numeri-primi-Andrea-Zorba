381394bc802f4133029bd8b7192f5842acc22f4b
Andrea-Zorba <92543460+Andrea-Zorba@users.noreply.github.com>
2021-11-30 22:01:48 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef ESERCITAZIONE_7_LIB_H
 #define ESERCITAZIONE_7_LIB_H
-void numeroprimo(int a, int b, int &conta);
-#endif // ESERCITAZIONE_7_LIB_H
+int funzione (int);
+#endif //ESERCITAZIONE_7_LIB_H
